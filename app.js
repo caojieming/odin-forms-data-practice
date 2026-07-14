b319fac14692222c8f4d72b847108b77f13e1641
James Cao <jcao5360@gmail.com>
2026-07-14 17:59:30 -0400

    implemented form + error message

--- app.js
+++ app.js
@@ -3,38 +3,20 @@ try {
   process.loadEnvFile();
 } catch(error) {}
 
-// express validator package import
-const { body, validationResult } = require("express-validator");
-
-// standard express import
 const express = require("express");
 const app = express();
-
-// import the Path CommonJS module from Node
 const path = require("node:path");
+const usersRouter = require("./routes/usersRouter");
 
-// let code know we're using ejs as template engine as well as location of views folder
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
-// setup path to static assets
-const assetsPath = path.join(__dirname, "public");
-app.use(express.static(assetsPath));
-
-// import routers
-const indexRouter = require("./routes/indexRouter");
-
-
 app.use(express.urlencoded({ extended: true }));
+app.use("/", usersRouter);
 
-app.use("/", indexRouter);
-
-
-// open web server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
   if (error) {
     throw error;
   }
-  console.log(`listening on port ${PORT}`);
+  console.log(`Express app listening on port ${PORT}!`);
 });
